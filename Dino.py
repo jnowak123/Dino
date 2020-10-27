48fab5ff8b0ed212be2ac7fe4397378b85b2fd0c
jacksokoleoko <jacksokoleoko@gmail.com>
2020-10-27 10:07:18 +0100

    Enenmy and auto enemy generating

--- Dino.py
+++ Dino.py
@@ -1,9 +1,12 @@
 import pymunkoptions
 pymunkoptions.options['debug'] = False #removes pymunk debug print from console
+import random
 import pyglet as pyg
 import pymunk as pym
 from pymunk.pyglet_util import DrawOptions
 from pyglet.window import FPSDisplay, key
+
+num = 0 
 
 class Player(pym.Body):
     def __init__(self, space):
@@ -11,6 +14,15 @@ class Player(pym.Body):
         self.position = 200, 140
         shape = pym.Poly.create_box(self, (40, 80))
         shape.elasticity = 0
+        space.add(self, shape)
+
+class Enemy(pym.Body):
+    def __init__(self, space):
+        super().__init__(10, pym.inf)
+        self.position = 1280, 140
+        self.velocity = -200, 0 # in the future, velocity will be equal to speed
+        self.elasticity = 0
+        shape = pym.Poly.create_box(self, (80, 40))
         space.add(self, shape)
 
 class Ground(pym.Body):
@@ -34,6 +46,7 @@ class Window(pyg.window.Window):
         self.player = Player(self.space)
         self.ground = Ground(self.space)
 
+
     def on_draw(self):
         self.clear()
         self.space.debug_draw(self.options) #drawing the pymunk space
@@ -45,6 +58,9 @@ class Window(pyg.window.Window):
 
     def update(self, dt): #date and time
         self.space.step(dt)
+        a = random.randint(0, 100)
+        if a == 1:
+            self.enemy = Enemy(self.space)
 
 window = Window(1280, 720, "Pymunk", resizable=False)
 pyg.clock.schedule_interval(window.update, 1/60)
