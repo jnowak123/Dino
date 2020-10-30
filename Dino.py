8f28b4fa3ced24c94a24b44d5c2d60e0476e47b3
jacksokoleoko <jacksokoleoko@gmail.com>
2020-10-30 16:25:28 +0100

    no double jumping, ducking only on ground and improved enemy generation

--- Dino.py
+++ Dino.py
@@ -10,12 +10,13 @@ from pyglet.gl import glClearColor
 class Player(pym.Body):
     def __init__(self, space, duck):
         super().__init__(10, pym.inf) #infinite moment => doesn't roll
-        self.position = 200, 40
         if duck:
             self.shape = pym.Poly.create_box(self, (40, 40))
+            self.position = 200, 40
         else:
             self.shape = pym.Poly.create_box(self, (40, 80))
-        self.shape.elasticity = 0.0
+            self.position = 200, 60
+        self.elasticity = 0.0
         space.add(self, self.shape)
 
 enemy_types = [(160,80), (40, 80), (40, 40), (40, 40), (120, 40)]
@@ -30,8 +31,8 @@ class Enemy(pym.Body):
         else:
             self.position = 1280, 40
         self.velocity = -200, 0 # in the future, velocity will be equal to speed
-        self.elasticity = 0.0
         shape = pym.Poly.create_box(self, enemy_types[a])
+        self.elasticity = 0.0
         space.add(self, shape)
 
 class Ground(pym.Body):
@@ -39,7 +40,7 @@ class Ground(pym.Body):
         super().__init__(10, pym.inf, pym.Body.STATIC) #static body => no gravity effect or moving at all
         self.position = 0, 0
         shape = pym.Poly.create_box(self, (2700, 40))
-        shape.elasticity = 0.0
+        self.elasticity = 0.0
         space.add(self, shape)
 
 class Window(pyg.window.Window):
@@ -57,28 +58,44 @@ class Window(pyg.window.Window):
         self.player = Player(self.space, False)
         self.ground = Ground(self.space)
 
+        self.sleep = 30
+        self.doing_duck = False
+
+    def position(self, body, dt):
+        pass
+
     def on_draw(self):
         self.clear()
         self.space.debug_draw(self.options) #drawing the pymunk space
         self.fps.draw()
 
     def on_key_press(self, symbol, modifiers):
-        if symbol == key.SPACE or symbol == key.UP:
-            self.player.velocity = 0, 600
-        elif symbol == key.DOWN:
-            self.space.remove(self.player.shape)
-            self.player = Player(self.space, True)
+        if round(self.player.position[1]/10)*10 == 60:
+            if symbol == key.SPACE or symbol == key.UP:
+                self.player.velocity = 0, 600
+            elif symbol == key.DOWN:
+                self.doing_duck = True
+                self.space.remove(self.player.shape)
+                self.player = Player(self.space, True)
 
     def on_key_release(self, symbol, modifiers):
-        if symbol == key.DOWN:
+        if symbol == key.SPACE or symbol == key.UP:
+            if self.player.velocity[1] > 0:
+                self.player.velocity = 0, 0
+        if symbol == key.DOWN and self.doing_duck:
+            self.doing_duck = False
             self.space.remove(self.player.shape)
             self.player = Player(self.space, False)
 
     def update(self, dt): #date and time
         self.space.step(dt)
-        a = random.randint(0, 80) # enemy generate
-        if a == 1:
+        self.sleep -= 1
+        if self.sleep == 0:
+            self.sleep = random.randint(90, 150) # enemy generate
             self.enemy = Enemy(self.space)
+    
+
+
 
 window = Window(1280, 720, 'Pymunk', resizable=False)
 pyg.clock.schedule_interval(window.update, 1/60)
