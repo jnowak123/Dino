b8f362dd964cb553fc046789109e3c0136fdb742
jacksokoleoko <jacksokoleoko@gmail.com>
2020-10-31 14:37:43 +0100

    first iteration of gravity

--- Dino_v2.py
+++ Dino_v2.py
@@ -6,7 +6,6 @@ class Game_Object():
         self.posx = posx
         self.posy = posy
         self.vely = 0
-        self.velx = 0
         if image is not None:
             image = pyg.image.load('sprites/' + image)
             self.sprite = pyg.sprite.Sprite(image, x=self.posx, y=self.posy)
@@ -16,7 +15,7 @@ class Game_Object():
 
     def update(self, dt):
         self.sprite.y += self.vely*dt
-        self.sprite.x += self.velx*dt
+        self.sprite.y -= 3
 
 class Window(pyg.window.Window):
     def __init__(self, *args, **kwargs): #arguments and keyword arguments
@@ -26,28 +25,21 @@ class Window(pyg.window.Window):
         self.fps = FPSDisplay(self)
 
         self.player = Game_Object(200, 40, 'dino.png')
-        self.player2 = Game_Object(500, 40, 'dino.png')
 
     def on_draw(self):
         self.clear()
         self.player.draw()
-        self.player2.draw()
 
     def on_key_press(self, symbol, modifiers):
         if symbol == key.SPACE or symbol == key.UP:
-            self.player.vely += 10
-        if symbol == key.RIGHT:
-            self.player.velx += 50
+            self.player.vely += 1000
 
     def on_key_release(self, symbol, modifiers):
         if symbol == key.SPACE or symbol == key.UP:
             self.player.vely = 0
-        if symbol == key.RIGHT:
-            self.player.velx = 0
 
     def update(self, dt):
         self.player.update(dt)
-        self.player2.update(dt)
 
 window = Window(1280, 720, 'Dino', resizable=False)
 pyg.clock.schedule_interval(window.update, window.framerate) #60 frames per second
